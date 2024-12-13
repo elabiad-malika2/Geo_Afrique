e50d7332bbbc1681b5a5e0b565bdd5160b705a12
Malika <elabiadmalika64@gmail.com>
2024-12-13 17:17:56 +0100

    ajouter la requete  edit pour editer une pays

--- ajouterP.php
+++ ajouterP.php
@@ -1,4 +1,16 @@
 <?php
+    include 'connexion.php';
+    if (isset($_GET['id'])) {
+        $nomP = $_POST["nom"];
+        $continentP=$_POST["continent"];
+        $languesP =$_POST["langues"];
+        $urlImageP =$_POST["urlImage"];
+        $populationP =$_POST["population"];
+        $idP=$_POST['id'];
+        $sql="UPDATE pays set nom='$nomP', population='$populationP', langues='$languesP', urlImage='$urlImageP', id_continent='$continentP' where id_pays = '$idP' ";
+        $conn->query($sql);
+        header("Location: pays.php");
+    }else{ 
     if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
         $nom = $_POST["nom"];
         $continent=$_POST["continent"];
@@ -13,5 +25,6 @@
 
         header("Location: pays.php");
 
+    }
     }
 ?>
