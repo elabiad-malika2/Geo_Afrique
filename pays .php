cb733c2ae7e141499b06dee1021d02c63867fbd6
Malika <elabiadmalika64@gmail.com>
2024-12-13 17:06:27 +0100

    ajout de header

--- pays .php
+++ pays .php
@@ -84,6 +84,30 @@ include 'connexion.php';
             </div>
         </div>
     </aside>
+    <div class="grow">
+    <header class=" h-20 border-b">
+            <nav class=" h-full flex justify-between mx-8 items-center">
+                <div class="flex gap-2">
+                    <img src="img/Search.svg" alt="">
+                    <input class="search outline-none border-none w-64 px-4 py-2 rounded-2xl" type="search" name="search-input" id="search-input" placeholder="Search anything here">
+                </div>
+                <div class="flex w-72 justify-between  items-center ">
+                    <img class="cursor-pointer" src="img/settings.svg" alt="">
+                    <img class="cursor-pointer" src="img/Icon.svg" alt="">
+                    <form action="" action="post">
+                        <button><img src="img/logout.png" class="h-4 w-4" alt=""></button>
+                    </form>
+                    <div class="flex items-center gap-2 cursor-pointer">
+                        <div class=" cursor-pointer w-10 h-10 bg-black bg-cover rounded-full text-white relative ">
+                        <div class="bg-[#228B22] h-3 w-3 rounded-full absolute bottom-0 right-0  "></div>
+                        </div>
+                    <p class="text-[#606060] font-bold">Malika </p>
+                    </div>
+                </div>
+    
+            </nav>
+        </header>
+    </div>
 </div>
 </body>
 </html>
